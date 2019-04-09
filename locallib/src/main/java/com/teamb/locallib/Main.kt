d38422e659c987cbd54e56ea68429d507e5a9102
Andrew <andrew@drewbitt.com>
2019-04-09 19:54:50 -0400

    Add functions for getting the parser

--- locallib/src/main/java/com/teamb/locallib/Main.kt
+++ locallib/src/main/java/com/teamb/locallib/Main.kt
@@ -1,8 +1,21 @@
 package com.teamb.locallib
 
 import com.teamb.locallib.parser.ParserInterface
+import com.teamb.locallib.streams.FileParserClass
 
 class Main {
 
     lateinit var parser: ParserInterface
+
+    fun initParser(path: String): Parser {
+        parser = FileParserClass(path).parser
+        return parser
+    }
+
+    fun cleanupParser() {
+        parser.path = ""
+        parser.destroy()
+    }
+
+    // maybe functions to get input streams
 }
